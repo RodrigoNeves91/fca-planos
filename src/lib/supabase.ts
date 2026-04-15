ebb1ac54acaa934e0e6add8635aad8e81df3aa31
RodrigoNeves91 <rodrigoluan@gmail.com>
2026-04-14 23:42:26 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://fidwxvehffegmcmfowml.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpZHd4dmVoZmZlZ21jbWZvd21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2OTc1NjMsImV4cCI6MjA5MTI3MzU2M30.o-G0-VC8lXwptaTYEePTMuy3qYDtT56r4_O3Ul7pj0M';
+const supabaseKey = 'sb_publishable_jUN_c4xhVpourT9KonM_aQ_dCTp0pe2';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
